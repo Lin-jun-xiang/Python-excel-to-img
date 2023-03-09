2ff641c86cd7533645cfab2123fdc7dc30f96881
JunXiang <63782903+Lin-jun-xiang@users.noreply.github.com>
2023-03-09 21:55:25 +0800

    Update xlsx2img.py

--- xlsx2img/xlsx2img.py
+++ xlsx2img/xlsx2img.py
@@ -10,25 +10,25 @@ def xlsx_to_img(excel_path: str, sheet_name: str, img_path: str) -> None:
     sheet_name: the sheet of xlsx file
     img_path: the output png file path
     """
-app = xw.App(visible=False, add_book=False)
+    app = xw.App(visible=False, add_book=False)
 
-wb = app.books.open(excel_path)
-sheet = wb.sheets(sheet_name)
+    wb = app.books.open(excel_path)
+    sheet = wb.sheets(sheet_name)
 
-all = sheet.used_range
-all.api.CopyPicture()
+    all = sheet.used_range
+    all.api.CopyPicture()
 
-sheet.api.Paste()
+    sheet.api.Paste()
 
-pic = sheet.pictures[-1]
-pic.api.Copy()
+    pic = sheet.pictures[-1]
+    pic.api.Copy()
 
-time.sleep(3)
+    time.sleep(3)
 
-img = ImageGrab.grabclipboard()
-img.save(img_path)
+    img = ImageGrab.grabclipboard()
+    img.save(img_path)
 
-pic.delete()
-wb.save(excel_path)
-wb.close()
-app.kill()
+    pic.delete()
+    wb.save(excel_path)
+    wb.close()
+    app.kill()
